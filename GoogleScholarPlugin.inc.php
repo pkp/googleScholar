8fb356ae7fce4147ec820557b6c80e3116483c68
ajnyga <ajnyga@gmail.com>
2020-03-23 21:50:09 +0200

    Fix abbrev output

--- GoogleScholarPlugin.inc.php
+++ GoogleScholarPlugin.inc.php
@@ -73,10 +73,10 @@ class GoogleScholarPlugin extends GenericPlugin {
 		// Context identification
 		if ($applicationName == "ojs2"){
 			$templateMgr->addHeader('googleScholarJournalTitle', '<meta name="citation_journal_title" content="' . htmlspecialchars($context->getName($context->getPrimaryLocale())) . '"/>');
-			if ($abbreviation = $context->getData('abbreviation', $context->getPrimaryLocale()) || $abbreviation = $context->getData('acronym', $context->getPrimaryLocale())) {
+			if (($abbreviation = $context->getData('abbreviation', $context->getPrimaryLocale())) || ($abbreviation = $context->getData('acronym', $context->getPrimaryLocale()))) {
 				$templateMgr->addHeader('googleScholarJournalAbbrev', '<meta name="citation_journal_abbrev" content="' . htmlspecialchars($abbreviation) . '"/>');
 			}
-			if (($issn = $context->getData('onlineIssn')) || ($issn = $context->getData('printIssn')) || ($issn = $context->getData('issn'))) {
+			if ( ($issn = $context->getData('onlineIssn')) || ($issn = $context->getData('printIssn')) || ($issn = $context->getData('issn'))) {
 				$templateMgr->addHeader('googleScholarIssn', '<meta name="citation_issn" content="' . htmlspecialchars($issn) . '"/> ');
 			}
 		}
