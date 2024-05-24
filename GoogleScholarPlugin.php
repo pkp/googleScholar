64ac00df476e2fa39fddce444a2bf93decd2fbeb
Jonas Raoni Soares da Silva <github@raoni.org>
2024-05-24 14:47:29 +0300

    Fixed data escape

--- GoogleScholarPlugin.php
+++ GoogleScholarPlugin.php
@@ -115,8 +115,8 @@ class GoogleScholarPlugin extends GenericPlugin
         $authors = $publication->getData('authors');
         foreach ($authors as $i => $author) {
             $templateMgr->addHeader('googleScholarAuthor' . $i, '<meta name="citation_author" content="' . htmlspecialchars($author->getFullName(false, false, $publicationLocale)) . '"/>');
-            if ($affiliation = htmlspecialchars($author->getLocalizedData('affiliation', $publicationLocale) ?? "")) {
-                $templateMgr->addHeader('googleScholarAuthor' . $i . 'Affiliation', '<meta name="citation_author_institution" content="' . $affiliation . '"/>');
+            if ($affiliation = $author->getLocalizedData('affiliation', $publicationLocale)) {
+                $templateMgr->addHeader('googleScholarAuthor' . $i . 'Affiliation', '<meta name="citation_author_institution" content="' . htmlspecialchars($affiliation) . '"/>');
             }
         }
 
