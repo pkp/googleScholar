649eaf6a6deed51125ce1a55acbbd6da20a3648e
GaziYucel <GaziYucel@users.noreply.github.com>
2024-11-05 15:00:02 +0100

    GoogleScholar

--- GoogleScholarPlugin.php
+++ GoogleScholarPlugin.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/generic/googleScholar/GoogleScholarPlugin.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2003-2020 John Willinsky
+ * Copyright (c) 2014-2024 Simon Fraser University
+ * Copyright (c) 2003-2024 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class GoogleScholarPlugin
@@ -115,7 +115,7 @@ class GoogleScholarPlugin extends GenericPlugin
         $authors = $publication->getData('authors');
         foreach ($authors as $i => $author) {
             $templateMgr->addHeader('googleScholarAuthor' . $i, '<meta name="citation_author" content="' . htmlspecialchars($author->getFullName(false, false, $publicationLocale)) . '"/>');
-            if ($affiliation = $author->getLocalizedData('affiliation', $publicationLocale)) {
+            if ($affiliation = $author->getLocalizedAffiliationNamesAsString($publicationLocale, ', ')) {
                 $templateMgr->addHeader('googleScholarAuthor' . $i . 'Affiliation', '<meta name="citation_author_institution" content="' . htmlspecialchars($affiliation) . '"/>');
             }
         }
