fff9a1add95f1d302775d7a0f8ed5ce5a33536de
GaziYucel <GaziYucel@users.noreply.github.com>
2024-11-07 14:23:00 +0100

    Multiple lines for affiliations

--- GoogleScholarPlugin.php
+++ GoogleScholarPlugin.php
@@ -115,8 +115,13 @@ class GoogleScholarPlugin extends GenericPlugin
         $authors = $publication->getData('authors');
         foreach ($authors as $i => $author) {
             $templateMgr->addHeader('googleScholarAuthor' . $i, '<meta name="citation_author" content="' . htmlspecialchars($author->getFullName(false, false, $publicationLocale)) . '"/>');
-            if ($affiliation = $author->getLocalizedAffiliationNamesAsString($publicationLocale, ', ')) {
-                $templateMgr->addHeader('googleScholarAuthor' . $i . 'Affiliation', '<meta name="citation_author_institution" content="' . htmlspecialchars($affiliation) . '"/>');
+            foreach($author->getLocalizedAffiliations() as $affiliation) {
+                if (!empty($affiliation['name'])) {
+                    $templateMgr->addHeader(
+                        'googleScholarAuthor' . $i . 'Affiliation' . $affiliation['id'],
+                        '<meta name="citation_author_institution" content="' . htmlspecialchars($affiliation['name']) . '"/>'
+                    );
+                }
             }
         }
 
